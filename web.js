1b1e8a48c2c74af0d354c88fef9c818969174ae2
Abhishek <abhishekhd@cmu.edu>
2013-09-02 08:14:07 +0000

    update-ver2 to web and index

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var app = express();
+var fs = require('fs');
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  var str = ("index.html");
+  response.send(str+'Hello World2!');
 });
 
 var port = process.env.PORT || 5000;
